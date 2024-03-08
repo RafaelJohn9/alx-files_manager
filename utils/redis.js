e7770ef8284795fd95eefe9e4b03b7082e3ac00d
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-08 20:11:06 +0300

    0.redis utils

---
+++ utils/redis.js
@@ -0,0 +1,69 @@
+/**
+ * redis module
+ */
+import * as redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    if (typeof (key) !== 'string') {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return (
+      new Promise((resolve, reject) => {
+        this.client.get(key, (err, reply) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(reply);
+          }
+        });
+      }));
+  }
+
+  async set(key, value, duration) {
+    if (typeof (key) !== 'string') {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    if (typeof (key) !== 'string') {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return (new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    }));
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
