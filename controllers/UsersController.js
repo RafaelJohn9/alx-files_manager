14b9f07e75b7698e7aa4307822a3fc59aad59fb6
RafaelJohn9 <rafaeljohb@gmail.com>
2024-03-11 22:13:48 +0300

    3.Corrected printout during adding items to users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,17 +7,23 @@ const UserController = {
   postNew: async (req, res) => {
     // Get the email and password from the request body
     const { email, password } = req.body ? req.body : [undefined, undefined];
+
     // If the email or password are missing, return a 400 status code
     if (!email || !password) {
       const missingElement = !email ? 'email' : 'password';
       res.status(400).json({ error: `Missing ${missingElement}` });
       return;
     }
+
     // Create a new user in the database
     const newUser = await dbClient.createUser(email, password);
+
     // If the user was created successfully, return a 201 status code
     if (newUser) {
-      res.status(201).json(newUser);
+      res.status(201).json({
+        id: newUser._id,
+        email: newUser.email,
+      });
     } else {
       res.status(400).json({ error: 'Already exists' });
     }
